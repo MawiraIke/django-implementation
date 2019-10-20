33885c3e3335a1e7fcf1683cc83d3e726948023b
MawiraIke <mawiraike@gmail.com>
2019-10-20 12:40:23 +0300

    Log in log out
    Signed-off-by: MawiraIkemawiraike@gmail.com <mawiraike@gmail.com>

--- hippo/urls.py
+++ hippo/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
     path('', HomePageView.as_view(), name='home'),
     path('post/new', NewPostView.as_view(), name='add_post'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail')]
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+]
