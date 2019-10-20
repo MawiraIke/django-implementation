1c33f2346edda1caa9618add91b75a552f2ff2a4
MawiraIke <mawiraike@gmail.com>
2019-10-20 11:58:43 +0300

    Delete post
    Signed-off-by: MawiraIkemawiraike@gmail.com <mawiraike@gmail.com>

--- hippo/urls.py
+++ hippo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView, NewPostView, PostDetailView, PostUpdateView
+from .views import (HomePageView, NewPostView, PostDetailView, PostUpdateView, PostDeleteView, )
 
 urlpatterns = [
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
     path('', HomePageView.as_view(), name='home'),
     path('post/new', NewPostView.as_view(), name='add_post'),
--- hippo/views.py
+++ hippo/views.py
@@ -1,5 +1,6 @@
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Post
 
@@ -25,3 +26,9 @@ class PostUpdateView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
